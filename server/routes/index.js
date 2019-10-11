d0f9c563c2bbf13e5845186baacb198f1d84a88a
richard4s <richardoluwo50@gmail.com>
2019-10-11 05:45:05 +0100

    Complete basic setup

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,4 +4,6 @@ module.exports = (app) => {
     app.get('/api', (req, res) => res.status(200).send({
         message: 'Kliining mocking test again api',
     }));
+
+    app.get('/api/mock', Users.mock);
 };
