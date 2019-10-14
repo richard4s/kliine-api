d16b7b971103d309969500a10ac2e9ae2137a3f3
richard4s <richardoluwo50@gmail.com>
2019-10-15 00:22:11 +0100

    Added sequelize CLI

--- server/config/config.js
+++ server/config/config.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 
 module.exports = {
   "development": {
-    "username": process.env.PG_USERNAME,
-    "password": process.env.PG_PASSWORD,
-    "database": process.env.PG_DB,
-    "host": process.env.PG_HOST,
+    "username": process.env.PG_USERNAME_DEV,
+    "password": process.env.PG_PASSWORD_DEV,
+    "database": process.env.PG_DB_DEV,
+    "host": process.env.PG_HOST_DEV,
     "dialect": "postgres"
   },
   "test": {
