2822eb49f68790ed8d62fd3e293e4476e9f523b1
richard4s <richardoluwo50@gmail.com>
2019-10-15 00:03:12 +0100

    Changed config production

--- server/config/config.js
+++ server/config/config.js
@@ -17,11 +17,10 @@ module.exports = {
     "operatorsAliases": false
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+    "username": process.env.PG_USERNAME,
+    "password": process.env.PG_PASSWORD,
+    "database": process.env.PG_DB,
+    "host": process.env.PG_HOST,
+    "dialect": "postgres"
   }
 }
